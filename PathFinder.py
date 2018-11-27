bb9a7dd3f3b82120391f1115bfdb56f301f06b84
Andrew Leonard <leonardishere@gmail.com>
2018-11-27 07:59:36 -0800

    create PathFinder

---
+++ PathFinder.py
@@ -0,0 +1,18 @@
+#find_path will find the path that data is located in
+#storing the data in different locations leads to errors between users
+#add your path to the paths array if yours is different
+
+import os
+
+def find_path():
+    paths = ["C:/Users/jon.harris/Desktop/school/final/input/", "data/", "../inputs/"]
+    my_path = ""
+    for path in paths:
+        try:
+            os.listdir(path)
+            my_path = path
+        except:
+            filler_exception=0
+    print("path: {}".format(my_path))
+    print("all files in directory: {}".format(os.listdir(my_path)))
+    return my_path
